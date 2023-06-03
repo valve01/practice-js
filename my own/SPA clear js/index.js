2948061096fd64b87e5e10f258a909d6c039cfc5
valve01 <118441831+valve01@users.noreply.github.com>
2023-06-03 19:52:16 +0300

    1

--- my own/SPA clear js/index.js
+++ my own/SPA clear js/index.js
@@ -1,5 +1,19 @@
 // Добавляем отслеживание переключения ссылок
-document.addEventListener("click",handler)
-handler= (e)=> {
-console.log(e)
+document.addEventListener("click", handler);
+function handler(e) {
+	// если элемент, по которому мы кликнули - это ссылка, то вызываем ф-цию route() и передаем в нее атрибут е (event). 
+	if (e.target.tagName === "A") {
+		route(e);
+	}
+	// Чтобы браузер не пытался перейти по ссылкам, метод preventDefault() вешаем на все событие
+	e.preventDefault();
 }
+
+// Опишем ф-цию роутинга, она принимает событие e от function handler, которая в свою очередь получила его от document.addEventListener
+const route = (e) => {
+// Обращаемся к объекту window и к его свойству history, и вызываем метод pushState
+
+	window.history.pushState({}, "", e.target.href);
+};
+// Заменяем стандартный роутинг объекта window на наш собственный (кастомный)
+window.route = route;
