98ac304937bd7230f95c2f3d934a6848d9ab0b84
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-06-24 20:48:03 +0300

    ы

---
+++ my own/подождать выполнение функции/main.js
@@ -0,0 +1,44 @@
+// ========================== Подождать выполнение функции. Организация Асинхронного кода =======================================================
+const secondF = () => console.log('second');
+const firstF = () => console.log('first');
+
+const callLis = async () => {
+
+	setTimeout(secondF, 1000);
+
+	let thenable = await {
+		then(resolve) {
+			setTimeout(() => resolve(firstF()), 3000);
+		},
+	}; 
+
+	setTimeout(secondF, 1000);
+};
+callLis();
+
+
+// ========================================== Подождать выполнение функции. Организация Асинхронного кода. Еще один пример ==========================
+// const summarize = (num1, num2) => num1 + num2;
+// const divide = (num1, num2) => num1 / num2;
+
+// let x = 2;
+// let y = 6;
+// let z = 10;
+
+// const calcAsync = async() => {
+
+//   x = summarize(x, x); // (1шаг) 2 + 2 = 4
+//   console.log('(1шаг)', x);
+
+//   x = await {
+//     then(resolve) {
+//       setTimeout(() => resolve(summarize(x, y)), 1000)
+//     }
+//   }; // (2шаг) ??? должно получится 4 + 6 = 10
+//   console.log('(2шаг)', x);
+
+//   x = divide(x, z); // (3шаг) ??? должно получится 10/10 = 1
+//   console.log('(3шаг)', x);
+// }
+
+// calcAsync();
