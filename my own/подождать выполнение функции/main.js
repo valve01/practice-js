9085f691703b4c0a4d0e7bf1f35f980d935c641a
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-06-24 20:48:24 +0300

    ы

--- my own/подождать выполнение функции/main.js
+++ my own/подождать выполнение функции/main.js
@@ -3,19 +3,17 @@ const secondF = () => console.log('second');
 const firstF = () => console.log('first');
 
 const callLis = async () => {
-
 	setTimeout(secondF, 1000);
 
 	let thenable = await {
 		then(resolve) {
 			setTimeout(() => resolve(firstF()), 3000);
 		},
-	}; 
+	};
 
 	setTimeout(secondF, 1000);
 };
 callLis();
-
 
 // ========================================== Подождать выполнение функции. Организация Асинхронного кода. Еще один пример ==========================
 // const summarize = (num1, num2) => num1 + num2;
@@ -39,6 +37,33 @@ callLis();
 
 //   x = divide(x, z); // (3шаг) ??? должно получится 10/10 = 1
 //   console.log('(3шаг)', x);
+// }
+
+// calcAsync();
+
+// ==========================Подождать выполнение функции. Организация Асинхронного кода. Третий пример (на промисах)==============================
+
+// const summarize = (num1, num2) => num1 + num2;
+// const divide = (num1, num2) => num1 / num2;
+
+// let x = 2;
+// let y = 6;
+// let z = 10;
+
+// const calcAsync = async() => {
+//   x = summarize(x, x);
+
+//   console.log(1, x);
+
+//   x = await new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(summarize(x, y)));
+//   });
+
+//   console.log(2, x);
+
+//   x = divide(x, z);
+
+//   console.log(3, x);
 // }
 
 // calcAsync();
